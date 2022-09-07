81f9cd8f75f8f67ddce353b5ea5030d387649977
William Dunston <wedunsto@aggies.ncat.edu>
2022-09-07 10:11:10 -0400

    Display an n number of token images

--- CustomTokens/src/screens/Results.js
+++ CustomTokens/src/screens/Results.js
@@ -2,49 +2,33 @@
 Show the results of the search criteria
 */
 import {React, useEffect, useState} from 'react';
-import {Text, StyleSheet, View, Image} from 'react-native';
+import {Text, StyleSheet, View, Image, FlatList} from 'react-native';
 var HTMLParser = require('fast-html-parser')
 
 const Results =({route})=>{
-    const [tokenURL, setTokenURL] = useState(null);
-    let imageURLs = []
+    const [tokenURLs, setTokenURLs] = useState(null);
 
     const {searchURL} = route.params
     useEffect(()=>{
         const loadTokens=async()=>{
             const response = await fetch(searchURL);
             const htmlString = await response.text();
+            
             try{
+                let imageURLs = []
                 var root = HTMLParser.parse(htmlString)
-                /*for(let i=0; i<root.childNodes[0].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[11].childNodes[3].childNodes.length; i++){
-                    console.log("==================================")
-                    console.log(i)
-                    console.log(root.childNodes[0].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[11].childNodes[3].childNodes[i])
-                    console.log("==================================")
-                }*/
-                //console.log("==================================")
-                //console.log(root.querySelectorAll('img').length)
-                //console.log(root.childNodes[0].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[11].childNodes[3])
-                //console.log(root.childNodes[0].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[11].childNodes[3].childNodes[1].childNodes[1].childNodes[1].childNodes[1])
-                //console.log("==================================")
                 
                 for(let i=0; i<root.querySelectorAll('img').length; i++){
                     if(root.querySelectorAll('img')[i].classNames.includes("cardSrc")){
-                        console.log("==================================")
-                        console.log(i)
-                        //console.log(root.querySelectorAll('img')[i].hasOwnProperty('class'))
-                        //console.log(Object.keys(root.querySelectorAll('img')[i]).length === 0)
-                        //console.log(Object.getOwnPropertyNames(root.querySelectorAll('img')[i]))
-                        console.log(root.querySelectorAll('img')[i])
-                        //console.log(root.querySelectorAll('img')[i])
-                        console.log("==================================")
+                        let rawAttrs = root.querySelectorAll('img')[i].rawAttrs
+                        let startIndex = rawAttrs.indexOf("https")
+                        let stopIndex = rawAttrs.indexOf("jpg")+3
+                        let imgObj = {id: i, src: rawAttrs.substring(startIndex, stopIndex)}
+                        imageURLs.push(imgObj)
                     }
                 }
-
-                let rawAttrs = root.childNodes[0].childNodes[3].childNodes[1].childNodes[5].childNodes[3].childNodes[11].childNodes[3].childNodes[1].childNodes[1].childNodes[1].childNodes[1].rawAttrs
-                let startIndex = rawAttrs.indexOf("https")
-                let stopIndex = rawAttrs.indexOf("jpg")+3
-                setTokenURL(rawAttrs.substring(startIndex, stopIndex))
+                
+                setTokenURLs(imageURLs)
             }
             catch(error){
                 console.error(error)
@@ -52,12 +36,19 @@ const Results =({route})=>{
         }
         loadTokens()
     }, []);
+
     return(
         <View style={styles.headerTextStyle}>
-            <Image 
-                    style={styles.buttonStyle}
-                    source={{uri: tokenURL}}
-                />
+            <FlatList
+                data={tokenURLs}
+                renderItem={({item})=>{
+                    return(<Image key={item.id}
+                        style={styles.buttonStyle}
+                        source={{uri: item.src}}
+                    />);
+                }}
+                keyExtractor={(items)=>{return items.id}}
+            />
         </View>
     );
 };
